400cccc9f89619d24d0604c6eb287404807d368c
Qwedfgr <georgiykvelidze@gmail.com>
2019-05-10 23:53:21 +0300

    добавил логирование

--- main.py
+++ main.py
@@ -2,13 +2,24 @@ import requests
 import telegram
 import time
 import os
+import logging
+import log_utils
+import dotenv
 
 
 def main():
+    dotenv.load_dotenv()
     token = os.environ["token"]
     chat_id = os.environ["chat_id"]
-    devman_token = os.environ["devman_token"]
-    bot = telegram.Bot(token=token)
+    devman_token = os.environ["devman_token"] + '1'
+    token_logger = os.environ['togen_log']
+
+    logger_bot = telegram.Bot(token=token_logger)
+    logger = logging.getLogger("bot-logger")
+    logger.setLevel(logging.INFO)
+    logger.addHandler(log_utils.MyLogsHandler(logger_bot, chat_id))
+    logger.info("Бот логер запущен!")
+
     url = 'https://dvmn.org/api/long_polling/'
     headers = {
         'Authorization': devman_token
@@ -17,8 +28,10 @@ def main():
     params = {
         'timestamp': timestamp
     }
+
     while True:
         try:
+            bot = telegram.Bot(token=token)
             response = requests.get(url, headers=headers, params=params)
             response.raise_for_status()
             response_data = response.json()
@@ -28,7 +41,16 @@ def main():
             else:
                 params['timestamp'] = response_data['timestamp_to_request']
         except (requests.exceptions.ConnectionError, requests.exceptions.ReadTimeout) as error:
+            logger.error('Ошибка соединения:')
+            logger.error(error)
             time.sleep(30)
+        except requests.exceptions.HTTPError as error:
+            logger.error(error)
+            break
+        except Exception as error:
+            logger.error('Бот упал с ошибкой:')
+            logger.error(error)
+            break
 
 
 def send_messages(bot, attempts, chat_id):
